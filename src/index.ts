cac16c13a43dad5851f73967db3338421cbefc6f
jimchen5209 <jimchen5209@gmail.com>
2021-07-29 09:50:37 +0800

    init ts project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export class Core {
+    constructor() {
+        // eslint-disable-next-line no-unused-expressions,@typescript-eslint/no-unused-expressions
+        // new Telegram();
+    }
+}
+
+// eslint-disable-next-line no-unused-expressions,@typescript-eslint/no-unused-expressions
+new Core();
